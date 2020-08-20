d7cb89abdc6d092c1c01241894fb754b4939d476
Carlos Nuila <cnuila14@icloud.com>
2020-08-20 15:16:26 -0600

    Agregando estudiantes y aulas

--- Proyecto_leng.pl
+++ Proyecto_leng.pl
@@ -1,4 +1,4 @@
-%aulas 40
+%aulas 62
 aula(nh101,28,12).
 aula(nh102,38,12).
 aula(nh103,25,15).
@@ -47,7 +47,34 @@ aula(etb108,25,15).
 aula(etb109,35,15).
 aula(etb110,40,25).
 
-%estudiantes 26
+aula(hta101,31,7).
+aula(hta102,20,0).
+aula(hta103,25,10).
+aula(hta104,30,6).
+aula(hta105,31,8).
+
+aula(hta106,15,4).
+aula(hta107,50,20).
+aula(hta108,20,7).
+aula(hta109,23,10).
+aula(hta110,17,2).
+
+aula(jpa101,23,10).
+aula(jpa102,40,20).
+aula(jpa103,47,13).
+aula(jpa104,19,6).
+aula(jpa105,26,7).
+
+aula(jpa106,40,15).
+aula(jpa107,33,11).
+aula(jpa108,15,0).
+aula(jpa109,22,4).
+aula(jpa110,31,15).
+aula(jpa111,18,2).
+aula(jpa112,34,15).
+
+%estudiantes 72
+
 estudiante(2000100001, 1, [math101, phys201, ec201]).
 estudiante(2000100002, 1, [math101, cmpe150, hist301]).
 estudiante(2000100003, 1, [math101, ec201, hist301]).
@@ -76,6 +103,57 @@ estudiante(2000100023, 0, [phys201, cmpe150]).
 estudiante(2000100024, 1, []).
 estudiante(2000100025, 0, [hist301]).
 estudiante(2000100026, 1, [ec201,math101,cmpe150]).
+
+estudiante(2000100027, 1, [phys201, ec201]).
+estudiante(2000100028, 1, [ux101, cmpe150, math101]).
+estudiante(2000100029, 0, [phys202, ec201, math201]).
+estudiante(2000100030, 0, [phys202, hist301]).
+estudiante(2000100031, 1, [ec201]).
+estudiante(2000100032, 0, [ux101]).
+estudiante(2000100033, 0, [math101, phys201, hist301,cmpe150]).
+estudiante(2000100034, 1, [ux101, phys202, hist301,cmpe150]).
+estudiante(2000100035, 0, [cmpe150, ec201]).
+estudiante(2000100036, 1, [cmpe150]).
+
+estudiante(2000100037, 1, [math101, phys202, hist301]).
+estudiante(2000100038, 0, [math101, phys201, ux101]).
+estudiante(2000100039, 1, [math201, ec201, hist301]).
+estudiante(2000100040, 1, [phys202, ec201]).
+estudiante(2000100041, 1, [hist301]).
+estudiante(2000100042, 1, [math101, phys201]).
+estudiante(2000100043, 0, [math101, phys201, hist301,cmpe150]).
+estudiante(2000100044, 1, [phys202]).
+estudiante(2000100045, 0, [ux101, ec201]).
+estudiante(2000100046, 1, []).
+
+estudiante(2000100047, 1, [math101]).
+estudiante(2000100048, 1, [math201, phys202, ux101]).
+estudiante(2000100049, 0, [math101, ec201, cmpe150]).
+estudiante(2000100050, 0, [phys201, ec201]).
+estudiante(2000100051, 0, [math201, phys202, ux101]).
+estudiante(2000100052, 0, [math101, ec201]).
+estudiante(2000100053, 0, [math101, phys201, hist301,cmpe150]).
+estudiante(2000100054, 1, [math201]).
+estudiante(2000100055, 0, [cmpe150, ux101]).
+estudiante(2000100056, 1, [math201,phys202]).
+
+estudiante(2000100057, 0, [math201, ux101, ec201]).
+estudiante(2000100058, 1, [math201, ec201, phys202]).
+estudiante(2000100059, 1, [math201, phys202, hist301]).
+estudiante(2000100060, 0, [phys201, hist301]).
+estudiante(2000100061, 0, [ux101]).
+estudiante(2000100062, 0, [math101, phys202]).
+estudiante(2000100063, 0, [math101, phys201, hist301,cmpe150]).
+estudiante(2000100064, 1, [math101]).
+estudiante(2000100065, 1, [cmpe150, ec201]).
+estudiante(2000100066, 1, [cmpe150,ux101]).
+
+estudiante(2000100067, 1, [math101, phys201, ec201]).
+estudiante(2000100068, 1, [math101, phys202]).
+estudiante(2000100069, 1, [math101, ec201, hist301]).
+estudiante(2000100070, 0, [hist301]).
+estudiante(2000100071, 1, [math101,hist301]).
+estudiante(2000100072, 0, [ux101, phys201]).
 
 %PARTE1  lista_estudiantes
 lista_estudiantes(S,L) :-setof(M,find(M,S),L).
